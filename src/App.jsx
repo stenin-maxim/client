2bdc8990ee8340ae7ddb082fc80a4c7a900d86e3
Maxim <stenin.dev@gmail.com>
2025-07-01 20:53:19 +0300

    Edited routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,44 @@
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Home from './pages/Home';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Restore from './pages/auth/Restore';
-import Profile from './pages/Profile';
-import { Routes, Route } from 'react-router'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router'
 import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import './scss/app.scss';
 import RequireAuth from './features/auth/RequireAuth'
+import RootLayout from './layout/RootLayout';
+import ProfileLayout from './layout/ProfileLayout';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Restore from './pages/auth/Restore';
+import Settings from './pages/profile/Settings';
+import ProfileInactive from './components/profile/ProfileInactive';
+import ProfileActive from './components/profile/ProfileActive';
 
 export default function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <div className="container">
-                    <Routes>
-                        {/* public routes */}
-                        <Route path="/" element={<Home/>} />
-                        <Route path="/login" element={<Login/>} />
-                        <Route path="/register" element={<Register/>} />
-                        <Route path="/restore" element={<Restore/>} />
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<RootLayout />}>
+                {/* public routes */}
+                <Route index element={<Home/>} />
+                <Route path="login" element={<Login/>} />
+                <Route path="register" element={<Register/>} />
+                <Route path="restore" element={<Restore/>} />
 
-                        {/* protected routes */}
-                        <Route element={<RequireAuth />}>
-                            <Route path="/profile" element={<Profile />} />
+                {/* protected routes */}
+                <Route element={<RequireAuth />}>
+                    <Route path="profile" element={<ProfileLayout />}>
+                        <Route element={<Profile />}>
+                            <Route path="active" element={<ProfileActive />} />
+                            <Route path="inactive" element={<ProfileInactive />} />
                         </Route>
-                    </Routes>
-                </div>
-            </main>
-            <Footer/>
-        </>
+                        <Route path="settings" element={<Settings />} />
+                    </Route>
+                </Route>
+            </Route>
+        )
+    );
+
+    return (
+        <RouterProvider router={router} />
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter} from 'react-router'
+// import { BrowserRouter} from 'react-router'
 import { Provider } from 'react-redux';
 import { store } from './app/store.js';
 import App from './App.jsx'
@@ -8,9 +8,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <App />
         </Provider>
     </React.StrictMode>
 )
