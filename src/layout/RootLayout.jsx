2cb14d38407cc17b569202418f2e67d84f9d83a6
Maxim <stenin.dev@gmail.com>
2025-07-27 15:35:19 +0300

    Add method sendVerificationEmail

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -5,10 +5,14 @@ import { logout } from '../features/auth/authSlice'
 import LocationModal from '../components/LocationModal';
 import Menu from '../components/Menu';
 import SearchForm from '../components/form/SearchForm';
+import Avatar from '../components/Avatar/Avatar';
 
 export default function RootLayout() {
     const [isVisibleMenuUser, setIsVisibleMenuUser] = useState(false);
+    const [isRequestPending, setIsRequestPending] = useState(false);
     const accessToken = useSelector((state) => state.auth.accessToken)
+    const user = useSelector(state => state.auth.user);
+    const avatar = useSelector(state => state.auth.avatar);
     const wrapperRef = useRef(null);
     const dispatch = useDispatch()
     const navigate = useNavigate();
@@ -45,9 +49,36 @@ export default function RootLayout() {
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [wrapperRef]);
-
+    
     const btnProduct = () => {
         accessToken ? navigate('/product') : alert('Авторизуйтесь, чтобы разместить обьявление')
+    }
+
+    const sendVerificationEmail = async () => {
+        try {            
+            if (!isRequestPending) {
+                setIsRequestPending(true);
+                const response = await fetch(`${baseUrl}email/verification-notification`, {
+                    method: 'POST',
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json'
+                    }
+                })
+
+                if (!response.ok) {
+                    setIsRequestPending(false);
+                    const errorData = await response.json();
+                    throw new Error(`Ошибка: ${errorData.message || 'Неизвестная ошибка'}`);
+                }
+                const data = await response.json();
+                alert(data.message);
+                setIsRequestPending(false);
+            }
+        } catch (error) {
+            alert(error.message);
+            setIsRequestPending(false);
+        }
     }
 
     return (
@@ -89,8 +120,10 @@ export default function RootLayout() {
                                     <SearchForm />
                                     <button className="btn" onClick={btnProduct}>Разместить обьявление</button>
                                     {accessToken && 
-                                    <div ref={wrapperRef}>
-                                        <div className="avatar" onClick={() => setIsVisibleMenuUser(!isVisibleMenuUser)}>М</div>
+                                    <div ref={wrapperRef} className="avatar-menu">
+                                        <div onClick={() => setIsVisibleMenuUser(!isVisibleMenuUser)}>
+                                            <Avatar src={avatar} name={user?.name} size="header" />
+                                        </div>
                                         {isVisibleMenuUser && (
                                             <ul className="menu-user">
                                                 <li><Link to="/profile">Мои объявления</Link></li>
@@ -110,6 +143,12 @@ export default function RootLayout() {
             </header>
             <main>
                 <div className="container">
+                    {user?.email_verified_at || !accessToken ? null : (
+                        <div className="warning">
+                            <h2>Вы не подтвердили свой email</h2>
+                            <button className="btn" onClick={sendVerificationEmail}>Отправить письмо повторно</button>
+                        </div>
+                    )}
                     <Outlet />
                 </div>
             </main>
