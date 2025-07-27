3d2071131e37ed629acac22c6aa3bfea3c826d44
Maxim <stenin.dev@gmail.com>
2025-07-27 15:34:57 +0300

    Add user and avatar in redux

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,21 +4,35 @@ const accessToken = localStorage.getItem('accessToken')
     ? localStorage.getItem('accessToken')
     : null
 
+const user = localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user'))
+    : null
+
 const authSlice = createSlice({
     name: 'auth',
-    initialState: { accessToken, error: null },
+    initialState: { accessToken, error: null, user, avatar: null },
     reducers: {
         setToken(state, action) {
             state.accessToken = action.payload;
             localStorage.setItem('accessToken', action.payload);
         },
+        setUser(state, action) {
+            state.user = action.payload;
+            localStorage.setItem('user', JSON.stringify(action.payload));
+        },
+        setAvatar(state, action) {
+            state.avatar = action.payload;
+            localStorage.setItem('avatar', action.payload);
+        },
         logout(state) {
             state.accessToken = null;
+            state.user = null;
             state.error = null;
             localStorage.removeItem('accessToken'); // delete token from storage
+            localStorage.removeItem('user');
         }
     }
 });
 
-export const { logout, setToken } = authSlice.actions;
+export const { logout, setToken, setUser, setAvatar } = authSlice.actions;
 export default authSlice.reducer;
