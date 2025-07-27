5e133c41c96692145adcdb8062bdab599e08c63b
Maxim <stenin.dev@gmail.com>
2025-07-27 15:19:59 +0300

    Created component Avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+
+const Avatar = ({ src, alt, name, size = "default", className = "" }) => {
+  const initial = name ? name[0] : "";
+  return src ? (
+    <img
+      src={src}
+      alt={alt || "avatar"}
+      className={classNames("avatar-img", `avatar-img--${size}`, className)}
+    />
+  ) : (
+    <div className={classNames("avatar", `avatar--${size}`, className)}>
+      {initial}
+    </div>
+  );
+};
+
+Avatar.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  name: PropTypes.string,
+  size: PropTypes.oneOf(["header", "sidebar", "settings", "default"]),
+  className: PropTypes.string,
+};
+
+export default Avatar;
