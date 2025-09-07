1f0cd6feb84cf5ef25a2d6cc4e6346d24d0c2fe5
Maxim <stenin.dev@gmail.com>
2025-09-07 16:23:24 +0300

    Add buttons delete and publish

--- src/components/profile/InactiveProfile.jsx
+++ src/components/profile/InactiveProfile.jsx
@@ -1,9 +1,42 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router';
+import { deleteUserProduct, publishUserProduct } from '../../api/userApi';
+import { updateUserProductValue, removeUserProduct } from '../../features/userProduct/userProductSlice';
 
 export default function InactiveProfile() {
+    const dispatch = useDispatch();
     const userProducts = useSelector(state => state.userProduct.userProducts);
+    const [loading, setLoading] = useState(false); // отменить многочисленное нажатие кнопки, при отправке данных
     let userProductsNoActive = userProducts?.filter((item) => item.status === 'noactive');
+
+    const publish = (productId) => {
+        setLoading(true);
+        publishUserProduct(productId).then(data => {
+            dispatch(updateUserProductValue({ id: productId, value: data.data.status }))
+            alert(data.message);
+        }).catch(error => {
+            console.log(error);
+        }).finally(function() {
+            setLoading(false);
+        })
+    }
+
+    const deleteProduct = (productId) => {
+        let result = confirm("Вы действительно хотите удалить объявление?");
+        setLoading(true);
+
+        if (result) {
+            deleteUserProduct(productId).then(data => {
+                dispatch(removeUserProduct(productId));
+                alert(data.message);
+            }).catch(error => {
+                console.log(error);
+            }).finally(function() {
+                setLoading(false);
+            })
+        }
+    }
 
     return (
         <>
@@ -17,9 +50,9 @@ export default function InactiveProfile() {
                                 </Link>
                             </div>
                             <div className="profile-item__desc">
-                                <h3>{item.name}</h3>
+                                <h3><Link to="">{item.name}</Link></h3>
                                 <h4 className="price">{item.price.toLocaleString('ru-RU')}</h4>
-                                <div>Кол-во:</div>
+                                <div>Кол-во: {item.amount} шт.</div>
                                 <div>{item.location}</div>
                             </div>
                         </div>
@@ -27,10 +60,10 @@ export default function InactiveProfile() {
                             <div className="profile-item__menu">
                                 <i className="bi bi-three-dots"></i>
                                 <ul>
-                                    <li>Опубликовать</li>
-                                    <li>Редактировать</li>
-                                    <li>Удалить</li>
-                                    <li>Уже не актульно</li>
+                                    <li><button onClick={() => publish(item.id)} disabled={loading}>Опубликовать</button></li>
+                                    <li><button disabled={loading}>Редактировать</button></li>
+                                    <li><button onClick={() => deleteProduct(item.id)} disabled={loading}>Удалить</button></li>
+                                    <li><button disabled={loading}>Уже не актульно</button></li>
                                 </ul>
                             </div>
                             <div className='profile-item__group'>
